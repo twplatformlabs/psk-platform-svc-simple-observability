238f5c8fb0c37b048c6905c3c8a168302b3adfb8
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-06-08 14:25:47 -0500

    [nc] generation scripts loki+dependencies
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/generate_obs_dependencies_application.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -euo pipefail
+source bash-functions.sh
+
+cluster_role=$1
+
+argocd_namespace=$(jq -er .argocd_namespace environments/$cluster_role.json)
+
+
+echo "Application resource and configuration files for on-cluster observability services"
+echo "role: $cluster_role"
+echo "creating deploy-files directory for obs-dependencies files that will written to psk-platform-control-plane-configuration repository"
+mkdir deploy-files
+mkdir deploy-files/obs-dependencies
+
+# generate application.yaml for both Applications then stage the files for writing to the app-of-app config repo
+echo "generating obs-dependencies application.yaml"
+cat <<EOF > deploy-files/obs-dependencies/application.yaml
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: obs-dependencies
+  namespace: $argocd_namespace
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  annotations:
+    argocd.argoproj.io/sync-wave: "2"
+spec:
+  project: psk-aws-control-plane-configuration
+
+  sources:
+    - repoURL: https://github.com/twplatformlabs/psk-platform-svc-simple-observability
+      path: chart/obs-dependencies
+      targetRevision: HEAD
+      helm:
+        valueFiles:
+          - \$config/roles/$cluster_role/obs-dependencies/deps-default-values.yaml
+    - repoURL: https://github.com/twplatformlabs/psk-aws-control-plane-configuration
+      targetRevision: HEAD
+      ref: config
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: observe
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - ServerSideApply=true
+      - SkipDryRunOnMissingResource=true
+    managedNamespaceMetadata:
+      labels:
+        app.kubernetes.io/managed-by: psk-platform-svc-simple-observability
+        platform-vault: "true"
+    retry:
+      limit: 5
+      backoff:
+        duration: 30s
+        factor: 2
+        maxDuration: 5m
+EOF
+cat deploy-files/obs-dependencies/application.yaml
+
+echo "copying default values"
+cp -v deploy-templates/deps-default-values.yaml deploy-files/obs-dependencies/deps-default-values.yaml
+
---
+++ scripts/generate_obs_loki_application.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -euo pipefail
+source bash-functions.sh
+
+cluster_role=$1
+
+loki_chart_version=$(jq -er .loki_chart_version environments/$cluster_role.json)
+argocd_namespace=$(jq -er .argocd_namespace environments/$cluster_role.json)
+
+# perform trivy scan of chart with role configuration.
+# ArgoCD Core will do the actual Helm install, this is just a pre-flight security review
+helm repo add grafana-community https://grafana-community.github.io/helm-charts
+helm repo update
+trivyScan "grafana-community/loki" "loki" "$loki_chart_version" "deploy-templates/loki-default-values.yaml"
+
+echo "Application resource and configuration files for crossplane"
+echo "loki chart version: $loki_chart_version"
+echo "creating deploy-files directory for all the loki files that will written to psk-platform-control-plane-configuration repository"
+mkdir deploy-files/obs-loki
+
+# generate application.yaml for both Applications then stage the files for writing to the app-of-app config repo
+echo "generating loki application.yaml"
+cat <<EOF > deploy-files/obs-loki/application.yaml
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: loki
+  namespace: $argocd_namespace
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  annotations:
+    argocd.argoproj.io/sync-wave: "3"
+spec:
+  project: psk-aws-control-plane-configuration
+
+  sources:
+    - repoURL: https://grafana-community.github.io/helm-charts
+      chart: loki
+      targetRevision: $loki_chart_version
+      helm:
+        valueFiles:
+          - \$config/roles/$cluster_role/obs-loki/loki-default-values.yaml
+          - \$config/roles/$cluster_role/obs-loki/loki-$cluster_role-values.yaml
+    - repoURL: https://github.com/twplatformlabs/psk-aws-control-plane-configuration
+      targetRevision: HEAD
+      ref: config
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: observe
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - ServerSideApply=true
+    retry:
+      limit: 5
+      backoff:
+        duration: 30s
+        factor: 2
+        maxDuration: 5m
+EOF
+cat deploy-files/obs-loki/application.yaml
+
+echo "copying default values"
+cp -v deploy-templates/loki-default-values.yaml deploy-files/obs-loki/loki-default-values.yaml
+cp -v deploy-templates/loki-$cluster_role-values.yaml deploy-files/obs-loki/loki-$cluster_role-values.yaml
